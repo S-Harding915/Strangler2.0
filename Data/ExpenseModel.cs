813092eee2324658e66568fcf25b310aa8195b19
khanimamba Ndhambi <khanimamban@agilebridge.co.za>
2025-05-17 12:17:39 +0200

    added budget component

---
+++ Data/ExpenseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Strangler2._0.Data
+{
+    public class ExpenseModel
+    {
+        public string CurrencySymbol { get; set; } = "$";
+        public decimal Budget { get; set; } = 0;
+    }
+}
